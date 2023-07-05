e45ce9e59eb6d21fed8fb4868593e4d94163a502
sandesh101 <rimal.sandesh11@gmail.com>
2023-07-05 08:03:38 +0545

    Designed UI for Slider Upload

--- lib/views/drawer_pages/slider_upload_page.dart
+++ lib/views/drawer_pages/slider_upload_page.dart
@@ -1,3 +1,4 @@
+import 'package:catering_service_adming/constant.dart';
 import 'package:flutter/material.dart';
 
 class SliderPage extends StatefulWidget {
@@ -12,7 +13,100 @@ class _SliderPageState extends State<SliderPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Upload Images'),
+        title: const Text('Upload Slider Images'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //Picked Image
+            Container(
+              height: MediaQuery.of(context).size.height * 0.3,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.red,
+              // child: ,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            //Button to pick image
+            GestureDetector(
+              onTap: () {
+                print('TAPPED');
+              },
+              child: Container(
+                height: 60,
+                width: 230,
+                decoration: BoxDecoration(
+                  color: ColorConstant.secondaryColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Text(
+                    "Pick Image",
+                    style: AppTextStyle.normalText(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Divider(
+              color: ColorConstant.blackColor,
+              thickness: 2,
+              indent: MediaQuery.of(context).size.width * 0.04,
+              endIndent: MediaQuery.of(context).size.width * 0.04,
+            ),
+            Align(
+              alignment: AlignmentDirectional.topStart,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(
+                  MediaQuery.of(context).size.width * 0.04,
+                  MediaQuery.of(context).size.height * 0.01,
+                  0.0,
+                  MediaQuery.of(context).size.height * 0.01,
+                ),
+                child: Text(
+                  'Current Slider Images',
+                  style: AppTextStyle.normalText(fontSize: 20),
+                ),
+              ),
+            ),
+            Divider(
+              color: ColorConstant.blackColor,
+              thickness: 2,
+              indent: MediaQuery.of(context).size.width * 0.04,
+              endIndent: MediaQuery.of(context).size.width * 0.04,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width,
+              child: GridView.builder(
+                  itemCount: 10,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: MediaQuery.of(context).size.height * 0.1,
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.red,
+                      ),
+                      child: const Center(child: Text("Hello")),
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
