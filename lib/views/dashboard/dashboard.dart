1cef0662e21a313992f907ab0cb868395010635c
Sandesh-0128 <sandeshrimal.whive@gmail.com>
2023-07-17 22:39:05 +0545

    Dashboard redesign

--- lib/views/dashboard/dashboard.dart
+++ lib/views/dashboard/dashboard.dart
@@ -13,12 +13,80 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: ColorConstant.secondaryColor,
-        title: Text("Dashboard", style: AppTextStyle.normalText(fontSize: 20)),
-        centerTitle: true,
-      ),
-      drawer: const AppDrawer(),
-    );
+        appBar: AppBar(
+          backgroundColor: ColorConstant.secondaryColor,
+          title:
+              Text("Dashboard", style: AppTextStyle.normalText(fontSize: 20)),
+          centerTitle: true,
+        ),
+        drawer: const AppDrawer(),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "3",
+                        style: AppTextStyle.boldText(
+                          color: ColorConstant.primaryColor,
+                          fontSize: 30,
+                        ),
+                      ),
+                      Text(
+                        "Orders",
+                        style: AppTextStyle.normalText(
+                            color: ColorConstant.primaryColor, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "3",
+                            style: AppTextStyle.boldText(
+                              color: ColorConstant.primaryColor,
+                              fontSize: 30,
+                            ),
+                          ),
+                          Text(
+                            "Orders",
+                            style: AppTextStyle.normalText(
+                                color: ColorConstant.primaryColor,
+                                fontSize: 18),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
