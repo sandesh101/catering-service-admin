456c1116577dbcd92b5f9d56bcd289c86e0e4924
sandesh101 <rimal.sandesh11@gmail.com>
2023-07-18 22:07:48 +0545

    Order Page Provider

--- lib/views/drawer_pages/order_page.dart
+++ lib/views/drawer_pages/order_page.dart
@@ -1,7 +1,9 @@
 import 'package:catering_service_adming/constant.dart';
+import 'package:catering_service_adming/provider/order_provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
 
 class OrderPage extends StatefulWidget {
   const OrderPage({super.key});
@@ -11,172 +13,161 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
-  late List<dynamic> orderList;
+  // late List<dynamic> orderList;
 
-  Future<void> getOrders() async {
-    CollectionReference ref = FirebaseFirestore.instance.collection('orders');
-    QuerySnapshot orederSnap = await ref.get();
+  // Future<void> getOrders() async {
+  //   CollectionReference ref = FirebaseFirestore.instance.collection('orders');
+  //   QuerySnapshot orederSnap = await ref.get();
 
-    orderList = orederSnap.docs.map((doc) => doc.data()).toList();
-    print("Orders: ${orderList[0]}");
-  }
+  //   orderList = orederSnap.docs.map((doc) => doc.data()).toList();
+  //   print("Orders: ${orderList[0]}");
+  // }
 
   @override
   void initState() {
     super.initState();
-    getOrders();
+    // getOrders();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      await Provider.of<OrderProvider>(context, listen: false).getOrders();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Order'),
-        centerTitle: true,
-      ),
-      body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('orders').snapshots(),
-          builder: ((context, snapshot) {
-            return snapshot.connectionState == ConnectionState.waiting
-                ? Center(
-                    child: CircularProgressIndicator(
-                      color: ColorConstant.blackColor,
-                    ),
-                  )
-                : ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: orderList.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.only(
-                            top: 8.0, left: 10.0, right: 10.0),
-                        child: Stack(
+        appBar: AppBar(
+          title: const Text('Order'),
+          centerTitle: true,
+        ),
+        body: Consumer<OrderProvider>(
+          builder: (context, orderProvider, _) => ListView.builder(
+              shrinkWrap: true,
+              itemCount: orderProvider.orderList.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.only(top: 8.0, left: 10.0, right: 10.0),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 200,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: ColorConstant.blackColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
                           children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 8.0, right: 8.0),
+                              child: Container(
+                                height: 50,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  // color: Colors.red,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Sandesh Rimal",
+                                      style: AppTextStyle.normalText(
+                                          color: ColorConstant.primaryColor),
+                                    ),
+                                    Row(
+                                      children: [
+                                        Icon(Iconsax.calendar_1,
+                                            color: ColorConstant.primaryColor),
+                                        const SizedBox(
+                                          width: 10,
+                                        ),
+                                        Text(
+                                          orderProvider.orderList[index]
+                                              ['date'],
+                                          style: AppTextStyle.normalText(
+                                              color:
+                                                  ColorConstant.primaryColor),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Iconsax.location,
+                                    color: ColorConstant.primaryColor),
+                                Text(
+                                  "Jarankhu, Kathmandu",
+                                  style: AppTextStyle.normalText(
+                                      fontSize: 18,
+                                      color: ColorConstant.primaryColor),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
                             Container(
-                              height: 200,
+                              height: 100,
                               width: double.infinity,
                               decoration: BoxDecoration(
-                                color: ColorConstant.blackColor,
+                                // color: Colors.red,
                                 borderRadius: BorderRadius.circular(10),
                               ),
                               child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 8.0, right: 8.0),
-                                    child: Container(
-                                      height: 50,
-                                      width: double.infinity,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        // color: Colors.red,
-                                      ),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            "Sandesh Rimal",
-                                            style: AppTextStyle.normalText(
-                                                color:
-                                                    ColorConstant.primaryColor),
-                                          ),
-                                          Row(
-                                            children: [
-                                              Icon(Iconsax.calendar_1,
+                                    padding: const EdgeInsets.only(left: 8.0),
+                                    child: Text(
+                                      "Food Items 👇",
+                                      style: AppTextStyle.normalText(
+                                          fontSize: 18,
+                                          color: ColorConstant.primaryColor),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: ListView.builder(
+                                        itemCount:
+                                            orderProvider.orderList.length,
+                                        itemBuilder: (context, index) {
+                                          return Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 40.0),
+                                            child: Text(
+                                              "- ${orderProvider.orderList[index]['food_items']}",
+                                              style: AppTextStyle.normalText(
                                                   color: ColorConstant
                                                       .primaryColor),
-                                              const SizedBox(
-                                                width: 10,
-                                              ),
-                                              Text(
-                                                orderList[index]['date'],
-                                                style: AppTextStyle.normalText(
-                                                    color: ColorConstant
-                                                        .primaryColor),
-                                              )
-                                            ],
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Icon(Iconsax.location,
-                                          color: ColorConstant.primaryColor),
-                                      Text(
-                                        "Jarankhu, Kathmandu",
-                                        style: AppTextStyle.normalText(
-                                            fontSize: 18,
-                                            color: ColorConstant.primaryColor),
-                                      )
-                                    ],
-                                  ),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                  Container(
-                                    height: 100,
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
-                                      // color: Colors.red,
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Padding(
-                                          padding:
-                                              const EdgeInsets.only(left: 8.0),
-                                          child: Text(
-                                            "Food Items 👇",
-                                            style: AppTextStyle.normalText(
-                                                fontSize: 18,
-                                                color:
-                                                    ColorConstant.primaryColor),
-                                          ),
-                                        ),
-                                        Expanded(
-                                          child: ListView.builder(
-                                              itemCount: orderList.length,
-                                              itemBuilder: (context, index) {
-                                                return Padding(
-                                                  padding:
-                                                      const EdgeInsets.only(
-                                                          left: 40.0),
-                                                  child: Text(
-                                                    "- ${orderList[index]['food_items']}",
-                                                    style:
-                                                        AppTextStyle.normalText(
-                                                            color: ColorConstant
-                                                                .primaryColor),
-                                                  ),
-                                                );
-                                              }),
-                                        )
-                                      ],
-                                    ),
-                                  ),
+                                            ),
+                                          );
+                                        }),
+                                  )
                                 ],
-                              ),
-                            ),
-                            const Positioned(
-                              top: 100,
-                              left: 200,
-                              child: Image(
-                                height: 100,
-                                image: AssetImage('assets/images/jeri.png'),
                               ),
                             ),
                           ],
                         ),
-                      );
-                    });
-          })),
-    );
+                      ),
+                      const Positioned(
+                        top: 100,
+                        left: 200,
+                        child: Image(
+                          height: 100,
+                          image: AssetImage('assets/images/jeri.png'),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+        ));
   }
 }
